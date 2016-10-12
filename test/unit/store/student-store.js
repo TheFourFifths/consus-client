473901e2bb6ca01ad8ce5ac55925040722d2eb18
Quentin Caffero <cafferoq@msoe.edu>
2016-10-12 04:07:25 -0500

    Added student store test to boost code coverage

--- test/unit/store/student-store.js
+++ test/unit/store/student-store.js
@@ -22,12 +22,20 @@ describe('StudentStore', () => {
         assert.strictEqual(student.name,'Poe');
     });
 
+    it('should handle a student not being found', () => {
+        Dispatcher.handleAction({type:'NO_STUDENT_FOUND'});
+        assert.strictEqual(StudentStore.getStudent(),null);
+    });
+
     it('should clear out the student when checkout succeeds',()=>{
         //First put student in the store
-        Dispatcher.handleAction('STUDENT_FOUND',{
-            id: '432345',
-            name: 'Poe',
-            items: [1,2,3,4,5]
+        Dispatcher.handleAction({
+            type:'STUDENT_FOUND',
+            data:{
+                id: '432345',
+                name: 'Poe',
+                items: [1,2,3,4,5]
+            }
         });
 
         Dispatcher.handleAction({type:'CHECKOUT_SUCCESS'});
