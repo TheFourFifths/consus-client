dcf636b08be951e74f3e0e7f8dfb5256ec0f5ed5
Quentin Caffero <cafferoq@msoe.edu>
2016-11-25 16:50:07 -0600

    Added timeout functionality

--- src/store/cart-store.js
+++ src/store/cart-store.js
@@ -1,26 +1,53 @@
 import { Store } from 'consus-core/flux';
 import StudentStore from './student-store';
+import { checkOutItems } from '../lib/api-client';
 
 let items = [];
+
+let timer = null;
+let isOnTimer = false;
+let TIMEOUT_TIME = 60000;
 
 class CartStore extends Store {
     getItems() {
         return items;
     }
+
+    isOnTimeout(){
+        return isOnTimer;
+    }
 }
 
 const store = new CartStore();
 
+store.registerHandler('STUDENT_FOUND', () => {
+    if(isOnTimer){
+        clearTimeout(timer);
+        isOnTimer = false;
+    }
+});
+
 store.registerHandler('CHECKOUT_ITEM_FOUND', data => {
+    if(timer !== null) {
+        clearTimeout(timer);
+    }
     let item = {
         address: data.address,
         status: data.status
     };
     items.push(item);
+    timer = setTimeout(() => {
+        checkOutItems(StudentStore.getStudent().id, items.map(item =>{return item.address;}));
+        isOnTimer = false;
+    }, TIMEOUT_TIME);
+    isOnTimer = true;
     store.emitChange();
 });
 
 store.registerHandler('CHECKOUT_SUCCESS', () => {
+    clearTimeout(timer);
+    isOnTimer = false;
+    timer = null;
     store.waitFor(StudentStore);
     items = [];
     store.emitChange();
