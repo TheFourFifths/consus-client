2c42d1ecc9db2c367919649d546bdc662911add7
Quentin Caffero <cafferoq@msoe.edu>
2016-12-01 14:36:23 -0600

    Made confirmation modal

---
+++ src/views/components/confirm-modal.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Modal from './modal.jsx';
+
+export default class ConfirmModal extends React.Component {
+
+
+    render() {
+        return (
+            <Modal
+                active={this.props.active}
+                buttonText="Yes"
+                onClose={() => this.props.onSelect(true)}>
+                <p>{this.props.message}</p><br/>
+                <br/>
+                {this.renderWarning()}
+                <button onClick={() => this.props.onSelect(false)}>No</button>
+            </Modal>
+        )
+    }
+}
