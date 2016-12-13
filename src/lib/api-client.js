b01dee8e51fa0ce8af853d215bcb6c4f00dd3561
Tony Fay <faya@msoe.edu>
2016-12-13 12:11:56 -0600

    Fixes so Boaty can live! (#51)
    * Made fixes to api-client and student.jsx that allow the checkout override to work properly
    
    * Fixed an import in api-client

--- src/lib/api-client.js
+++ src/lib/api-client.js
@@ -2,7 +2,6 @@ import request from 'request';
 import { Dispatcher } from 'consus-core/flux';
 import { hashHistory } from 'react-router';
 import AuthStore from '../store/authentication-store';
-import StudentStore  from '../store/student-store';
 
 function get(endpoint, data) {
     let options = {
@@ -138,11 +137,6 @@ export function searchItem(address) {
 }
 
 export function searchItemForCheckout(address) {
-    if(StudentStore.getStudent().hasOverdueItem) {
-        return Dispatcher.handleAction('ERROR', {
-            error:'Student has at least one overdue item.'
-        });
-    }
     get('item', {
         address
     }).then(data => {
