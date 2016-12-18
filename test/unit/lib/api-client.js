b46f4c418ca613bf81d52d936cbf8a71552f422a
Trevin Hofmann <trevinhofmann@gmail.com>
2016-12-17 23:43:41 -0600

    Add a test for createItem

--- test/unit/lib/api-client.js
+++ test/unit/lib/api-client.js
@@ -1,6 +1,11 @@
 import { assert } from 'chai';
 import MockServer from '../../util/mock-server';
-import { changePort, checkIn, checkOutItems } from '../../../.dist/lib/api-client';
+import {
+    changePort,
+    checkIn,
+    checkOutItems,
+    createItem
+} from '../../../.dist/lib/api-client';
 import util from 'util';
 
 describe('API Client', () => {
@@ -49,6 +54,29 @@ describe('API Client', () => {
             MockServer.validate({
                 studentId: '123456',
                 itemAddresses: ['iGwEZUvfA', 'iGwEZVHHE']
+            });
+        });
+    });
+
+    it('createItem', () => {
+        let response = {
+            status: 'success',
+            data: {
+                address: 'iGwEZUvfA',
+                modelName: 'Resistor'
+            }
+        };
+        return MockServer.listen({
+            port: 8080,
+            method: 'post',
+            endpoint: '/api/item',
+            response
+        }).then(() => {
+            return createItem('m8y7nEtAe');
+        }).then(data => {
+            assert.deepEqual(data, response.data);
+            MockServer.validate({
+                modelAddress: 'm8y7nEtAe'
             });
         });
     });
