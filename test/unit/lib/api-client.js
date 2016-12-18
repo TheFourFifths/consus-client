b913782876cd8d92581d7dfb21e55788baf36994
Trevin Hofmann <trevinhofmann@gmail.com>
2016-12-17 22:52:53 -0600

    Replace '.includes', not supported in Node v4

--- test/unit/lib/api-client.js
+++ test/unit/lib/api-client.js
@@ -20,24 +20,19 @@ describe('API Client', () => {
                 }
             }
         };
-        util.inspect('listen');
         return MockServer.listen({
             port: 8080,
             method: 'post',
             endpoint: '/api/checkin',
             response
         }).then(() => {
-            util.inspect('checkin');
             return checkIn('123456', 'iGwEZUvfA');
         }).then(data => {
-            util.inspect('deep equal');
             assert.deepEqual(data, response.data);
-            util.inspect('validate');
             MockServer.validate({
                 studentId: '123456',
                 itemAddress: 'iGwEZUvfA'
             });
-            util.inspect('done');
         });
     });
 
