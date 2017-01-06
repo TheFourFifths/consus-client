17f4b2d933039b165f5c7507bcdbd546a075e979
Quentin Caffero <cafferoq@msoe.edu>
2017-01-06 11:22:49 -0600

    Item store test

--- src/controllers/pages/overdue.js
+++ src/controllers/pages/overdue.js
@@ -4,7 +4,6 @@ import { Dispatcher } from 'consus-core/flux';
 export default class OverdueItemsController {
     static getOverdueItems() {
         return getOverdueItems().then(items => {
-            console.log('Got Overdue:', items);
             Dispatcher.handleAction("OVERDUE_ITEMS_RECEIVED", items);
         });
     }
