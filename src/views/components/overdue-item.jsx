4b1f1c4242a34d34d219ce644b0884cfe2e52c2d
Quentin Caffero <cafferoq@msoe.edu>
2017-01-16 15:54:48 -0600

    more styling

--- src/views/components/overdue-item.jsx
+++ src/views/components/overdue-item.jsx
@@ -22,18 +22,24 @@ export default class OverdueItem extends React.Component {
         if (this.state.item === null)
             return <i>Item is loading...</i>;
         return (
-            <div className='overdue-item'>
+            <div className='overdueItem'>
                 <div className="picArea">
                     <img src="../assets/images/placeholder.jpg"/>
                 </div>
-                <div className="titleArea">
-                    <h2>{this.state.model.name}</h2><h4>{this.state.model.address}</h4>
-                    <p>Item Address: {this.state.item.address}</p>
+                <div className="modelItemArea">
+                    <div className="modelArea">
+                        <h3>{this.state.model.name}</h3><p>{this.state.model.address}</p>
+                    </div>
+                    <div className="itemArea">
+                        <h3>Address:</h3><p>{this.state.item.address}</p>
+                    </div>
                 </div>
                 <div className="infoArea">
                     <div className="descriptionArea">
                         <h3>Checked Out To:</h3>
                         <p>{this.state.item.student.name} ({this.state.item.student.id})</p><br/>
+                    </div>
+                    <div className="dueOn">
                         <h3>Due on:</h3>
                         <p>{dueOn.toDateString()}</p>
                     </div>
