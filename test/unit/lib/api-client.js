c8995f5d0ceaf584868d3130799758fd0dde4de1
Quentin Caffero <cafferoq@msoe.edu>
2017-01-18 11:31:12 -0600

    Model edit delete story (#79)
    * Started making controllers
    
    * Updated cart-panel to use cart controller
    
    * Create item form controller
    
    * ModelFormController
    
    * ItemController
    
    * ModelController
    
    * Omnibar Controller
    
    * Removed unused components
    
    * Missing semicolons
    
    * Removed commented-out code
    
    * First attempt to test controllers
    
    * Continued attempt to test controller"
    
    * Finished first controller test. And fixed some small bugs
    
    * CartController tests
    
    * tightened up the test for cart panel
    
    * ItemFormController tests
    
    * fixed tests
    
    * ModelFormController tests
    
    * ItemController tests
    
    * ModelController tests
    
    * OmnibarController tests
    
    * IndexController Tests. (Over the 50 test markgit add src test)
    
    * ItemController tested"
    
    * Added deleteItem to ItemController
    
    * Added tests for deleteItem in ItemController
    
    * Added StudentController tests and other small things
    
    * Fixed PR comments
    
    * Missed .bind
    
    * Delete a model and its items
    
    * Fix some spy issues and not returning promise in test.
    
    * Remove console statement
    
    * Update model.js
    
    To error.message
    
    * error.message
    
    * Fix the error message
    
    * Switch to have two calls for deleting model.
    
    * Add toast for deleting a model, switch call to be two calls one to delete then one to update the store.
    
    * Editing a model.
    
    * remove console.og
    
    * some tests
    
    * Api tests
    
    * PR review comments and api tests
    
    * change patch to use both query and json
    
    * Fixup tests to have both JSON and QS
    
    * .
    
    * .
    
    * an attempt
    
    * .
    
    * Add confirmation modal to delete model
    
    * Proper promise

--- test/unit/lib/api-client.js
+++ test/unit/lib/api-client.js
@@ -14,6 +14,8 @@ import {
     searchItem,
     searchModel,
     searchStudent,
+    updateModel,
+    deleteModel,
     uploadStudents
 } from '../../../.dist/lib/api-client';
 
@@ -47,7 +49,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'post',
             endpoint: '/api/checkin',
-            request: {
+            json: {
                 studentId: '123456',
                 itemAddress: 'iGwEZUvfA'
             },
@@ -66,7 +68,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'post',
             endpoint: '/api/checkout',
-            request: {
+            json: {
                 studentId: '123456',
                 itemAddresses: ['iGwEZUvfA', 'iGwEZVHHE']
             },
@@ -85,7 +87,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'post',
             endpoint: '/api/checkout',
-            request: {
+            json: {
                 studentId: '123456',
                 itemAddresses: ['iGwEZUvfA', 'iGwEZVHHE'],
                 adminCode: 'abcdef'
@@ -109,7 +111,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'post',
             endpoint: '/api/item',
-            request: {
+            json: {
                 modelAddress: 'm8y7nEtAe'
             },
             response
@@ -139,7 +141,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'post',
             endpoint: '/api/model',
-            request: {
+            json: {
                 name: 'Resistor',
                 description: 'V = IR',
                 manufacturer: 'Live',
@@ -173,7 +175,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'delete',
             endpoint: '/api/item',
-            request: {
+            qs: {
                 itemAddress: 'iGwEZUvfA',
                 modelAddress: 'm8y7nEtAe'
             },
@@ -202,7 +204,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'get',
             endpoint: '/api/item/all',
-            request: {},
+            qs: {},
             response
         });
         return getAllItems().then(data => {
@@ -232,7 +234,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'get',
             endpoint: '/api/model/all',
-            request: {},
+            qs: {},
             response
         });
         return getAllModels().then(data => {
@@ -255,7 +257,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'get',
             endpoint: '/api/item',
-            request: {
+            qs: {
                 address: 'iGwEZUvfA'
             },
             response
@@ -287,12 +289,44 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'get',
             endpoint: '/api/model',
-            request: {
+            qs: {
                 address: 'm8y7nEtAe'
             },
             response
         });
         return searchModel('m8y7nEtAe').then(data => {
+            assert.deepEqual(data, response.data);
+            mockServer.validate();
+        });
+    });
+
+    it('deleteModel', () => {
+        let response = {
+            status: 'success',
+            data: {
+                model: {
+                    address: 'm8y7nEtAe',
+                    name: 'Resistor',
+                    description: 'V = IR',
+                    manufacturer: 'Pancakes R Us',
+                    vendor: 'Mouzer',
+                    location: 'Shelf 14',
+                    isFaulty: false,
+                    faultDescription: '',
+                    price: 10.50,
+                    count: 20
+                }
+            }
+        };
+        mockServer.expect({
+            method: 'delete',
+            endpoint: '/api/model',
+            qs: {
+                modelAddress: 'm8y7nEtAe'
+            },
+            response
+        });
+        return deleteModel('m8y7nEtAe').then(data => {
             assert.deepEqual(data, response.data);
             mockServer.validate();
         });
@@ -312,7 +346,7 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'get',
             endpoint: '/api/student',
-            request: {
+            qs: {
                 id: '123456'
             },
             response
@@ -330,12 +364,63 @@ describe('API Client', () => {
         mockServer.expect({
             method: 'post',
             endpoint: '/api/student',
-            request: {
+            json: {
                 data: '123456'
             },
             response
         });
         return uploadStudents('123456').then(data => {
+            assert.deepEqual(data, response.data);
+            mockServer.validate();
+        });
+    });
+
+    it('updateModel', () => {
+        let response = {
+            status: 'success',
+            data: {
+                address: 'm8y7nEtAe',
+                name: 'Resistor',
+                description: 'V = IR',
+                manufacturer: 'Pancakes R Us',
+                vendor: 'Mouzer',
+                location: 'Shelf 14',
+                isFaulty: false,
+                faultDescription: '',
+                price: 10.50,
+                count: 20,
+                items: [ "iGwEZUvfA", "iGwEZVHHE", "iGwEZVeaT"]
+            }
+        };
+        mockServer.expect({
+            method: 'patch',
+            endpoint: '/api/model',
+            qs: {
+                address: 'm8y7nEtAe'
+            },
+            json: {
+                name: 'Resistor',
+                description: 'V = IR',
+                manufacturer: 'Pancakes R Us',
+                vendor: 'Mouzer',
+                location: 'Shelf 14',
+                isFaulty: false,
+                faultDescription: '',
+                price: 10.50
+            },
+            response
+        });
+        return updateModel(
+            'm8y7nEtAe',
+            'Resistor',
+            'V = IR',
+            'Pancakes R Us',
+            'Mouzer',
+            'Shelf 14',
+            false,
+            '',
+            10.50,
+        ).then(data => {
             assert.deepEqual(data, response.data);
             mockServer.validate();
         });
