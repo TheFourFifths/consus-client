91b56235ad66c6d6cf4c1e072b978449b7e4a13d
Quentin Caffero <cafferoq@msoe.edu>
2017-01-18 10:37:16 -0600

    Overdue page styling tff 152 (#78)
    * Initial run at styling
    
    * more styling
    
    * Styled Overdue items page
    
    * Kill the redundant home button

--- src/views/components/overdue-item.jsx
+++ src/views/components/overdue-item.jsx
@@ -22,18 +22,24 @@ export default class OverdueItem extends React.Component {
         if (this.state.item === null)
             return <i>Item is loading...</i>;
         return (
-            <div className='overdue-item'>
+            <div className='overdueItem'>
                 <div className="picArea">
                     <img src="../assets/images/placeholder.jpg"/>
                 </div>
-                <div className="titleArea">
-                    <h2>{this.state.model.name}</h2><h4>{this.state.model.address}</h4>
-                    <p>Item Address: {this.state.item.address}</p>
+                <div className="modelItemArea  displayBlock">
+                    <div className="modelArea subBlock">
+                        <h3>{this.state.model.name}</h3><p>({this.state.model.address})</p>
+                    </div>
+                    <div className="itemArea subBlock">
+                        <h3>Item Address:</h3><p>{this.state.item.address}</p>
+                    </div>
                 </div>
-                <div className="infoArea">
-                    <div className="descriptionArea">
+                <div className="infoArea  displayBlock">
+                    <div className="descriptionArea subBlock">
                         <h3>Checked Out To:</h3>
                         <p>{this.state.item.student.name} ({this.state.item.student.id})</p><br/>
+                    </div>
+                    <div className="dueOn subBlock">
                         <h3>Due on:</h3>
                         <p>{dueOn.toDateString()}</p>
                     </div>
