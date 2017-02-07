ca24ec54ed3fa4b640845ff2c8ee4c0b624f46d8
Trevin Hofmann <trevinhofmann@gmail.com>
2017-02-07 10:55:16 -0600

    Remove qr margin

--- src/lib/qr.js
+++ src/lib/qr.js
@@ -2,6 +2,7 @@ import qr from 'qr-image';
 
 export function getDataUri(text, pixelsPerSquare = 10) {
     return 'data:image/png;base64,' + qr.imageSync(text, {
-        size: pixelsPerSquare
+        size: pixelsPerSquare,
+        margin: 0
     }).toString('base64');
 }
