701eb6012e03217f92a0ebeda0ec122623fb32ce
longatoj <longatoj@msoe.edu>
2017-03-06 15:44:56 -0600

    unit tests

--- test/unit/controllers/components/model.js
+++ test/unit/controllers/components/model.js
@@ -47,4 +47,47 @@ describe("ModelController", () => {
             searchModel.restore();
         });
     });
+
+    describe.only('addItemToModel', () => {
+        let dispatcherSpy, createItem;
+        beforeEach(() => {
+            dispatcherSpy = sinon.spy(Dispatcher, "handleAction");
+            createItem = sinon.stub(api, "createItem");
+        });
+
+        it('Dispatches "ITEM_CREATED" when item is created for model', () => {
+            let modelAddress = 'fake';
+            createItem.returns(
+                new Promise((resolve, reject) => {
+                    resolve('nothing');
+                })
+            );
+
+            return ModelController.addItemToModel(modelAddress).then(() => {
+                assert.isTrue(dispatcherSpy.called);
+                assert.strictEqual(dispatcherSpy.getCall(0).args.length, 2);
+                assert.strictEqual(dispatcherSpy.getCall(0).args[0], "ITEM_CREATED");
+            });
+        });
+
+        it('Dispatches "ERROR" when item is created for model', () => {
+            let modelAddress = 'fake';
+            createItem.returns(
+                new Promise((resolve, reject) => {
+                    reject('nothing');
+                })
+            );
+
+            return ModelController.addItemToModel(modelAddress).then(() => {
+                assert.isTrue(dispatcherSpy.called);
+                assert.strictEqual(dispatcherSpy.getCall(0).args.length, 2);
+                assert.strictEqual(dispatcherSpy.getCall(0).args[0], "ERROR");
+            });
+        });
+
+        afterEach(() => {
+            dispatcherSpy.restore();
+            createItem.restore();
+        });
+    });
 });
