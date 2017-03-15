adcd5ad087f95db7d4e44aa051943188673747b5
Matthew Mahnke <mahnkem@msoe.edu>
2017-03-15 01:49:11 -0500

    :green_heart: Fix poor merge

--- src/views/components/model.jsx
+++ src/views/components/model.jsx
@@ -40,6 +40,12 @@ export default class Model extends React.Component {
         });
     }
 
+    showDeleteConfirmModal(){
+        this.setState({
+            needsConfirmationForDelete: true
+        });
+    }
+
     showAddItemConfirmModal(){
         this.setState({
             needsConfirmationForAdd: true
