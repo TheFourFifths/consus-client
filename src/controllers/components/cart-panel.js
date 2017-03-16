afcfedec2be33568e77286056039eecc12f92998
Tony Fay <faya@msoe.edu>
2017-03-16 08:50:01 -0500

    Fixed minor bug with adding serialized model to cart

--- src/controllers/components/cart-panel.js
+++ src/controllers/components/cart-panel.js
@@ -34,14 +34,14 @@ export default class CartController {
 
     static getModel(address) {
         return searchModel(address).then(model => {
-            if (model.inStock <= 0) {
-                return Dispatcher.handleAction('ERROR', {
-                    error: `${model.name} is out of stock.`
-                });
-            }
             if(!model.allowCheckout) {
                 return Dispatcher.handleAction('ERROR', {
                     error: `${model.name} is not available for checkout.`
+                });
+            }
+            if (model.inStock <= 0) {
+                return Dispatcher.handleAction('ERROR', {
+                    error: `${model.name} is out of stock.`
                 });
             }
             Dispatcher.handleAction("CHECKOUT_MODEL_FOUND", model);
