9ddeae7cdc7d0deed2a3a6b29fd5473ab8932833
Trevin Hofmann <trevinhofmann@gmail.com>
2017-03-28 11:45:49 -0500

    More time for timeout test

--- test/unit/store/cart-store.js
+++ test/unit/store/cart-store.js
@@ -69,7 +69,7 @@ describe('CartStore', () => {
         setTimeout(() => {
             assert.isFalse(CartStore.isOnTimeout());
             done();
-        }, CartStore.TIMEOUT_TIME);
+        }, 2 * CartStore.TIMEOUT_TIME);
     });
 
     it('times out for models', function(done){
@@ -89,7 +89,7 @@ describe('CartStore', () => {
         setTimeout(() => {
             assert.isFalse(CartStore.isOnTimeout());
             done();
-        }, CartStore.TIMEOUT_TIME);
+        }, 2 * CartStore.TIMEOUT_TIME);
     });
 
     it('cancels timeout when new student is scanned', () => {
