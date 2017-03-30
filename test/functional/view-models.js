18a7c1a4f64ee347c0f1c6935df3e79c129e6a3c
Tony Fay <faya@msoe.edu>
2017-03-29 19:49:48 -0500

    Added asserts to functional test

--- test/functional/view-models.js
+++ test/functional/view-models.js
@@ -4,7 +4,7 @@ import { assert } from 'chai';
 import MockServer from '../util/mock-server';
 import models from '../test-cases/models';
 
-describe('View all models', function () {
+describe.only('View all models', function () {
 
     this.timeout(10000);
     let app;
@@ -127,12 +127,16 @@ describe('View all models', function () {
         return app.client.click(`#addbtn${models[2].address}`).then(() => {
             return app.client.waitForVisible('.modal', 5000);
         }).then(() => {
+            return app.client.getText('.modal');
+        }).then(modalText => {
+            assert.include(modalText, 'Add another Resistor?');
             return app.client.click('.modal .modal-content button[type="button"]');
         }).then(()=> {
             return app.client.waitForVisible('.toast', 1000);
         }).then(() => {
-            return app.client.getText(`#${models[2].address} .infoArea`);
-        }).then(modelList => {
+            return app.client.getText('.toast');
+        }).then(toastText => {
+            assert.include(toastText, 'New Resistor (m8y7nFnMs) created');
             mockServer.validate();
         });
     });
