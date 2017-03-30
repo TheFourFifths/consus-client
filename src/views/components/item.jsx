36d80c1f982951ece2c0a7bed5e18991aaaee287
Quentin Caffero <cafferoq@msoe.edu>
2017-03-30 00:32:06 -0500

    display fault history on item tff218 (#118)
    * display fault history on item
    
    * Slight ui change
    
    * Format timestamps
    
    * This file does not belong
    
    * a milli
    
    * Change timezone back to America/Chicago
    
    * Kill seconds

--- src/views/components/item.jsx
+++ src/views/components/item.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router';
 import ItemController from '../../controllers/components/item';
 import PrinterController from '../../controllers/pages/printer';
 import ModelController from '../../controllers/pages/model';
+import config from 'config';
+import moment from 'moment-timezone';
 
 export default class Item extends React.Component {
 
@@ -68,7 +70,7 @@ export default class Item extends React.Component {
                     </div>
                     <div key={'' + this.state.item.address + this.state.item.faultHistory.length}
                      className="faultArea">
-                        <h3>Fault</h3>
+                        <h3>Current Fault</h3>
                         {(this.state.item.isFaulty
                                 ? <span><button onClick={() => ItemController.removeItemFault(this.state.item.address)}>ClearFault</button><p>{this.state.item.faultHistory[0].description}</p></span>
                                 : <p>Item is not currently faulty.</p>
@@ -81,7 +83,11 @@ export default class Item extends React.Component {
                     </div>
                     <div className="faultHistory">
                         <h3>Fault History:</h3>
-                        {this.state.item.faultHistory.length}
+                        {this.state.item.faultHistory.length === 0? <p>No fault history for this item</p> :(this.state.item.faultHistory.map((fault,index) => {
+                            if (index === 0 && this.state.item.isFaulty) return null;
+                            let timestamp = moment.tz(fault.timestamp * 1000, config.get('timezone')).format('MMM D Y, h:mm a');
+                            return <div key={fault.description + fault.timestamp + new Date().getTime()}>{timestamp}: {fault.description}</div>;
+                        }))}
                     </div>
                 </div>
                 <div className="actionArea">
