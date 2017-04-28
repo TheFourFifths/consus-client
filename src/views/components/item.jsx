8f9db1f8bdd6eb065e105570b582284194d51e69
Tony Fay <faya@msoe.edu>
2017-04-28 10:16:06 -0500

    Styled the items

--- src/views/components/item.jsx
+++ src/views/components/item.jsx
@@ -40,6 +40,10 @@ export default class Item extends React.Component {
         }
     }
 
+    goToModel(address, e) {
+        ItemController.goToModel(address);
+    }
+
     addFault() {
         if (this.state.faultBoxOpen)
             ItemController.addFault({
@@ -73,9 +77,18 @@ export default class Item extends React.Component {
         PrinterController.promptToPrint(this.state.item.address);
     }
 
+    formatStatusString(status) {
+        let words = status.split("_");
+        let result = "";
+        for(let word of words) {
+            result += (word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() + " ");
+        }
+        return result.slice(0, -1);
+    }
+
     render() {
         if (this.state.item === null)
-            return <i>Item is loading...</i>;
+            return (<i>Item is loading...</i>);
         let deleteConfirmationText = `Are you sure you want to delete this item (${this.state.item.address})?`;
         return (
             <div className='item'>
@@ -88,27 +101,29 @@ export default class Item extends React.Component {
                     <img src={`data:image/jpeg;base64,${this.state.model.photo}`}/>
                 </div>
                 <div className="titleArea">
-                    <strong><Link to={`/model/${this.state.model.address}`}>{this.state.model.name}</Link></strong><br/>
+                    <strong><span className='itemName' onClick={this.goToModel.bind(this, this.state.model.address)}>{this.state.model.name}</span></strong><br/>
                     <i>{this.state.item.address}<br/></i>
-                    <strong>Status:</strong> {this.state.item.status}
+                    <span className={`status ${this.state.item.status}`}>{this.formatStatusString(this.state.item.status)}</span>
                 </div>
-                <div className="infoArea">
-                    <div key={'' + this.state.item.address + this.state.item.faultHistory.length} className="faultArea">
-                        <h3>Current Fault</h3>
-                        {(this.state.item.isFaulty
-                                ? <span><button onClick={() => ItemController.removeItemFault(this.state.item.address)}>ClearFault</button><p>{this.state.item.faultHistory[0].description}</p></span>
-                                : <p>Item is not currently faulty.</p>
-                        )}
-
-                        <br/>
-                        {this.state.faultBoxOpen ? <input ref={'' + this.state.item.address + 'fault'} /> : <br /> }
-                        <button className={this.state.faultBoxOpen ? "saveButton" : "addFault" } onClick={this.addFault.bind(this)}>{this.state.faultBoxOpen ? "Save Fault" : "Add Fault" }</button>
-                        {this.state.faultBoxOpen ? <button onClick={this.cancelFault.bind(this)}> Cancel </button> : ""}
+                <div className="faultArea">
+                    <div className="faultInput" key={'' + this.state.item.address + this.state.item.faultHistory.length}>
+                        <h3>Current Fault:</h3>
+                        <div className='currentFault'>
+                            {(this.state.item.isFaulty
+                                    ? <span><img className='icon-button' onClick={() => ItemController.removeItemFault(this.state.item.address)} src="../assets/images/clear.svg" title="Remove Fault"/>{this.state.item.faultHistory[0].description}</span>
+                                    : (this.state.faultBoxOpen ?
+                                        <span><input className='faultTextBox' ref={'' + this.state.item.address + 'fault'} placeholder="Enter Fault" autoFocus />
+                                        <img className='icon-button' onClick={this.cancelFault.bind(this)} src="../assets/images/cancel.svg" title="Cancel" />
+                                        <img className='icon-button' onClick={this.addFault.bind(this)} src="../assets/images/check.svg" title="Save" /></span>
+                                        : <span><img className='icon-button' onClick={this.addFault.bind(this)} src="../assets/images/add-3.svg" title="Add Fault" /><i>Item is not currently faulty.</i></span>
+                                    )
+                            )}
+                        </div>
                     </div>
                     <div className="faultHistory">
                         <h3>Fault History:</h3>
                         <div className="faults">
-                            {this.state.item.faultHistory.length === 0? <p>No fault history for this item</p> :(this.state.item.faultHistory.map((fault, index) => {
+                            {this.state.item.faultHistory.length === 0 ? <i>No fault history for this item</i> :(this.state.item.faultHistory.map((fault, index) => {
                                 if (index === 0 && this.state.item.isFaulty) return null;
                                 let timestamp = moment.tz(fault.timestamp * 1000, config.get('timezone')).format('MMM D Y');
                                 return <div key={fault.description + fault.timestamp + new Date().getTime()}>{timestamp}: {fault.description}</div>;
@@ -117,12 +132,11 @@ export default class Item extends React.Component {
                     </div>
                 </div>
                 <div className="actionArea">
-                    <img onClick={this.showDeleteConfirmModal.bind(this)} src="../assets/images/delete.svg"/>
-                    <img onClick={this.openQr.bind(this)} src='../assets/images/qr.svg' />
+                    <img className='icon-button' onClick={this.showDeleteConfirmModal.bind(this)} src="../assets/images/delete.svg"/>
+                    <img className='icon-button' onClick={this.openQr.bind(this)} src='../assets/images/qr.svg' />
                 </div>
                 <div className="clear"></div>
             </div>
         );
     }
-
 }
