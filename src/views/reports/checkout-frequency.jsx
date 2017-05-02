80889ea5df4a702a2b8913fbdce66698133aebc0
Quentin Caffero <cafferoq@msoe.edu>
2017-05-02 09:48:04 -0500

    More elegant loading and sorting functionality

--- src/views/reports/checkout-frequency.jsx
+++ src/views/reports/checkout-frequency.jsx
@@ -6,9 +6,20 @@ import ModelStore from '../../store/model-store';
 import ReportModelWithFrequency from '../components/report-model-with-frequency.jsx';
 
 export default class CheckoutFrequencyReportPage extends ListenerComponent {
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: true,
+            sortBy: "Name"
+        };
+    }
 
     componentWillMount(){
-        CheckoutFrequencyReportPageController.getAllItems();
+        CheckoutFrequencyReportPageController.getAllItems().then(() => this.setState({loading:false}));
+    }
+
+    changeSort(e){
+        this.setState({sortBy: e.target.id});
     }
 
     getStores(){
@@ -25,12 +36,21 @@ export default class CheckoutFrequencyReportPage extends ListenerComponent {
         };
     }
 
+    getSortFunction(){
+        switch(this.state.sortBy){
+            case "Frequency":
+                return function (a, b){return a.frequency < b.frequency;}
+            case "Name":
+                return function (a, b){return a.name.localeCompare(b.name);}
+            case "LastCheckout":
+                return function (a, b){return a.lastCheckedOut < b.lastCheckedOut;}
+        }
+    }
 
     render() {
+        if(this.state.loading) return <span>Loading Report...</span>;
 
-        function sortByFrequency(a, b){
-            return a.frequency < b.frequency;
-        }
+        let sorter = this.getSortFunction();
 
         return (
             <div>
@@ -38,13 +58,13 @@ export default class CheckoutFrequencyReportPage extends ListenerComponent {
                 <table className="checkout-frequency-report-table" key={this.state.models.length}>
                     <thead>
                         <tr>
-                            <th>Model Name (Address)</th>
-                            <th>Times Checked Out</th>
-                            <th>Last Checkout Date</th>
+                            <th id="Name" onClick={this.changeSort.bind(this)}>Model Name (Address)</th>
+                            <th id="Frequency" onClick={this.changeSort.bind(this)}>Times Checked Out</th>
+                            <th id="LastCheckout" onClick={this.changeSort.bind(this)}>Last Checkout Date</th>
                         </tr>
                     </thead>
                     <tbody>
-                    {this.state.models.sort(sortByFrequency).map(model => {
+                    {this.state.models.sort(sorter).map(model => {
                         return(
                             <ReportModelWithFrequency key={model.address + model.frequency} model={model}/>
                         );
